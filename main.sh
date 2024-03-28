97535fb6ac18ecd7e6328e55b66c702e5364b1d9
Uroš Grandovec <uros.grandovec@preskok.si>
2024-03-28 09:13:01 +0100

    update parameters.yaml and imports

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#SBATCH -p valhala                  # partition (queue)
+#SBATCH -p gpu                    # partition (queue)
 #SBATCH --qos=valhala
 #SBATCH -N 1                      # number of nodes
-#SBATCH -n 1                      # number of cores
-##SBATCH --mem=2M                 # memory pool for all cores
-#SBATCH -t 0-00:01                # time (D-HH:MM)
+#SBATCH -n 10                     # number of cores
+##SBATCH --mem=10G                # memory pool for all cores
+#SBATCH -t 1-00:00                # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out        # STDOUT
 #SBATCH -e slurm.%N.%j.err        # STDERR
 
 source ../rnn_generator_env/bin/activate
 
-python optimize.py --optimization_steps 5 --models_per_step 10
+python /rnn_autoregression_model/optimize.py --optimization_steps 5 --models_per_step 10
