bbe08b7be0159ab8f2c9c5bff6002d8ff50fe570
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-06 16:30:28 +0200

    another fix

--- main_r.sh
+++ main_r.sh
@@ -3,7 +3,7 @@
 #SBATCH -p gpu                    # partition (queue)
 #SBATCH --qos=valhala
 #SBATCH --nodes=2                 # number of nodes
-#SBATCH --ntasks-per-node=4       # number of cores
+#SBATCH --ntasks-per-node=2       # number of cores
 #SBATCH --mem=10G                 # memory pool for all cores
 #SBATCH -t 1-00:00                # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out        # STDOUT
@@ -13,4 +13,4 @@ source rnn_generator_env/bin/activate
 
 cd rnn_autoregression_model
 
-srun python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 4 --accelerator gpu
+srun python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 2 --accelerator gpu
