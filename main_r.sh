7f6c7e287cf05add12eb896f20b820870a2ba12a
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-06 19:57:42 +0200

    Update memory pool size in main_r.sh

--- main_r.sh
+++ main_r.sh
@@ -4,7 +4,7 @@
 #SBATCH --qos=valhala
 #SBATCH --nodes=2                 # number of nodes
 #SBATCH --ntasks-per-node=2       # number of cores
-#SBATCH --mem=10G                 # memory pool for all cores
+#SBATCH --mem=5G                 # memory pool for all cores
 #SBATCH -t 1-00:00                # time (D-HH:MM)
 #SBATCH -o slurm.%N.%j.out        # STDOUT
 #SBATCH -e slurm.%N.%j.err        # STDERR
@@ -13,4 +13,4 @@ source rnn_generator_env/bin/activate
 
 cd rnn_autoregression_model
 
-srun python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 2 --accelerator gpu
+python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 2 --accelerator gpu
