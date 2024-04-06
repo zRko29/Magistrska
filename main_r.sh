0f2e04166ababf35a1c6ecdf05f82bc540e4d3c3
Uroš Grandovec <uros.grandovec@preskok.si>
2024-04-06 20:04:23 +0200

    Fix srun command in main_r.sh

--- main_r.sh
+++ main_r.sh
@@ -13,4 +13,4 @@ source rnn_generator_env/bin/activate
 
 cd rnn_autoregression_model
 
-python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 2 --accelerator gpu
+srun python optimize.py --optimization_steps 2 --models_per_step 1 --num_nodes 2 --strategy ddp --num_devices 2 --accelerator gpu
